e948707a5fe5a03b89c38d24574be021ef6daae0
rielisson <118862671+rielisson@users.noreply.github.com>
2025-05-29 14:08:49 -0300

    hero e navbar prontos

--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import './Hero.css';
 import imagemRestaurante from '../../assets/freepik__upload__65781.png';
 
-function Hero () {
+function Hero() {
     return (
         <div className="hero">
             <div className="div-image-principal">
-                <img className="image-principal" src={imagemRestaurante} alt="imagem de um restaurante moderno"/>
+                <img className="image-principal" src={imagemRestaurante} alt="imagem de um restaurante moderno" />
             </div>
-            <h1>Bella Cucina</h1>
-            <p>
-                Authentic italian cuisine made with lova and the finest ingredients
-            </p>
-            <button>View Menu</button>
-            <button>Make a Reservation</button>
-
+            <div className="hero-informacoes">
+                <h1 className="title-hero">Bella Cucina</h1>
+                <p className="text-hero">
+                    Authentic italian cuisine made with lova and the finest ingredients
+                </p>
+                <div className="div-buttons">
+                    <button className="button-view">View Menu</button>
+                    <button className="button-resevation">Make a Reservation</button>
+                </div>
+            </div>
         </div>
     )
 }
